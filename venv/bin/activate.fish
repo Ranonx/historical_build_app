020c54bd95de26d9925c5d7116ac17caf2486190
Ranonx <108208227+Ranonx@users.noreply.github.com>
2024-04-12 11:49:03 +0800

    added git ignore

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/chowranon/Library/Mobile Documents/com~apple~CloudDocs/Documents/Personal ⛺️/Personal Improvement/Master of CS/my_flask_app/venv"
+set -gx VIRTUAL_ENV "/Users/chowranon/Library/Mobile Documents/com~apple~CloudDocs/Documents/Personal ⛺️/Personal Improvement/Master of CS/hist-app/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
