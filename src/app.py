afbd5d7abb3ff19539c1c4576a2c922a784d3caf
Ranonx <108208227+Ranonx@users.noreply.github.com>
2024-04-12 12:30:31 +0800

    updated my whole app

--- src/app.py
+++ src/app.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
-     </form>
-     '''
+    return render_template('home.html'),200
 
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+
+    return render_template('response.html',input_text=input_text),200
